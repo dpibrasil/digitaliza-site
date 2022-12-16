3ad77d5845f16f71c5b3b73aca57b421d8fef67e
Erivaldo Cavalcante <cavalcanteerivaldo0@gmail.com>
2022-12-15 23:13:02 -0300

    correção da responsividade

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,12 +16,12 @@ function Header()
                 <button onClick={handleChangeMenuOpen} className="md:hidden ml-32 mt-12">
                     <FaBars />
                 </button>
-                <nav className={menuOpen ? 'flex flex-col items-start justify-between h-32' : 'hidden'}>
-                    <a href="/#" className="text-[#16C194] font-bold text-sm lg:text-lg ml-6">Serviços</a>
-                    <a href="/#" className="ml-6 lg:ml-10 text-[#16C194] font-bold text-sm lg:text-lg">GED</a>
-                    <a href="/#" className="ml-6 lg:ml-10 text-[#16C194] font-bold text-sm lg:text-lg">Digitaliza Cloud</a>
-                    <a href="/#" className="ml-6 lg:ml-10 text-[#16C194] font-bold text-sm lg:text-lg">Sobre nós</a>
-                    <button className="ml-6 lg:ml-10 mr-6 lg:mr-12 px-8 py-2 rounded bg-black text-white font-bold text-sm lg:text-lg">Entrar</button>
+                <nav className={menuOpen ? 'flex flex-col items-start justify-between h-32' : 'hidden md:block'}>
+                    <a href="/#" className="text-[#16C194] font-bold text-sm lg:text-lg ml-5">Serviços</a>
+                    <a href="/#" className="ml-5 lg:ml-10 text-[#16C194] font-bold text-sm lg:text-lg">GED</a>
+                    <a href="/#" className="ml-5 lg:ml-10 text-[#16C194] font-bold text-sm lg:text-lg">Digitaliza Cloud</a>
+                    <a href="/#" className="ml-5 lg:ml-10 text-[#16C194] font-bold text-sm lg:text-lg">Sobre nós</a>
+                    <button className="ml-5 lg:ml-10 mr-5 lg:mr-12 px-8 py-2 rounded bg-black text-white font-bold text-sm lg:text-lg">Entrar</button>
                     <button>
                         <FaTimes className="hidden md:hidden" />
                     </button>
